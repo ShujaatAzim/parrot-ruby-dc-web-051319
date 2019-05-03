802f349c2deca1f1d40c1e167c2be34b56709016
Shujaat Azim <shujaat.azim@gmail.com>
2019-05-03 00:35:18 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(str = "Squak!")
-  puts str
-  return str
+def parrot(string = "Squak!")
+  puts string
+  return string
 end
